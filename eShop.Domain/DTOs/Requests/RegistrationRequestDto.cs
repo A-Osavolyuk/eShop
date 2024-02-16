0e9cf72c31ffbbbf5df644b4e24414133781744c
A.Osavoliuk <sasha.osavoll11@gmail.com>
2024-02-16 21:30:28 +0200

    Added validation for RegistrationRequestDto

---
+++ eShop.Domain/DTOs/Requests/RegistrationRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace eShop.Domain.DTOs.Requests
+{
+    public class RegistrationRequestDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
