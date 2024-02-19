974dd517c3091deb0e28182e00fbc28eadc20b05
A.Osavoliuk <sasha.osavoll11@gmail.com>
2024-02-19 12:04:34 +0200

    Added services to main class

--- eShop.ProductWebApi/Program.cs
+++ eShop.ProductWebApi/Program.cs
@@ -1,23 +1,26 @@
+using eShop.ProductWebApi;
+using eShop.ProductWebApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.AddApiServices();
 
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var context = scope.ServiceProvider.GetRequiredService<ProductDbContext>();
+        context.Database.EnsureCreated();
+    }
 }
 
 app.UseHttpsRedirection();
