a94cd9d854b993ee24f07a991ee265da0bfa5bd6
A.Osavoliuk <sasha.osavoll11@gmail.com>
2024-02-19 12:03:34 +0200

    Added SQL connection to Product_DB

--- eShop.ProductWebApi/Extensions.cs
+++ eShop.ProductWebApi/Extensions.cs
@@ -1,7 +1,30 @@
-﻿namespace eShop.ProductWebApi
-{
-    public class Extensions
-    {
+﻿using eShop.Application;
+using eShop.ProductWebApi.Data;
 
+namespace eShop.ProductWebApi
+{
+    public static class Extensions
+    {
+        public static IHostApplicationBuilder AddApiServices(this IHostApplicationBuilder builder)
+        {
+            builder.AddServiceDefaults();
+            builder.AddApiApplicationLayer();
+            builder.AddJwtAuthentication();
+
+            builder.AddDependencyInjection();
+
+            builder.AddSqlServerDbContext<ProductDbContext>("SqlServer");
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            return builder;
+        }
+
+        public static IHostApplicationBuilder AddDependencyInjection(this IHostApplicationBuilder builder)
+        {
+            return builder;
+        }
     }
 }
