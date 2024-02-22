2b1f670ab25e3aa3bc7f6cc049184ea2f61cd113
A.Osavoliuk <sasha.osavoll11@gmail.com>
2024-02-22 16:35:17 +0200

    Added ProductCategory validation

---
+++ eShop.Application/Validation/ProductCategoryValidator.cs
@@ -0,0 +1,16 @@
+﻿using eShop.Domain.DTOs.Requests;
+using FluentValidation;
+
+namespace eShop.Application.Validation
+{
+    public class ProductCategoryValidator : AbstractValidator<ProductCategoryDto>
+    {
+        public ProductCategoryValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("Name is must!")
+                .MaximumLength(32).WithMessage("Name cannot be longer then 32 characters")
+                .MinimumLength(3).WithMessage("Name must contain at least 3 characters");
+        }
+    }
+}
