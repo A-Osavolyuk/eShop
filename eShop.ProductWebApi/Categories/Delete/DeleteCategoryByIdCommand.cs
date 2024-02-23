ac2f7b06cd4e2e6d67d1d553e0c1024517d1d280
A.Osavoliuk <sasha.osavoll11@gmail.com>
2024-02-23 16:56:42 +0200

    Added DELETE category logic

---
+++ eShop.ProductWebApi/Categories/Delete/DeleteCategoryByIdCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace eShop.ProductWebApi.Categories.Delete
+{
+    public record DeleteCategoryByIdCommand(Guid Id) : IRequest<Result<bool>>;
+
+    public class DeleteCategoryByIdCommandHandler : IRequestHandler<DeleteCategoryByIdCommand, Result<bool>>
+    {
+        private readonly ICategoriesRepository repository;
+
+        public DeleteCategoryByIdCommandHandler(ICategoriesRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<Result<bool>> Handle(DeleteCategoryByIdCommand request, CancellationToken cancellationToken)
+        {
+            var result = await repository.DeleteCategoryByIdAsync(request.Id);
+
+            return result.Match<Result<bool>>(s => new(s), f => new(f));
+        }
+    }
+}
