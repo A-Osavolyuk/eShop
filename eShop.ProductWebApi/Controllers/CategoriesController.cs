7a36082e08ab43d52cceb3d4ef2e5ce38f218d0f
A.Osavoliuk <sasha.osavoll11@gmail.com>
2024-02-29 12:39:43 +0200

    Added PUT categories methods

--- eShop.ProductWebApi/Controllers/CategoriesController.cs
+++ eShop.ProductWebApi/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using eShop.ProductWebApi.Categories.Create;
 using eShop.ProductWebApi.Categories.Delete;
 using eShop.ProductWebApi.Categories.Get;
+using eShop.ProductWebApi.Categories.Update;
 
 namespace eShop.ProductWebApi.Controllers
 {
@@ -103,6 +104,39 @@ namespace eShop.ProductWebApi.Controllers
                             .Failed()
                             .AddErrorMessage(exception.Message)
                             .Build());
+
+                    return StatusCode(500, new ResponseBuilder()
+                        .Failed()
+                        .AddErrorMessage(f.Message)
+                        .Build());
+                });
+        }
+
+        [HttpPut("{Id:guid}")]
+        public async ValueTask<ActionResult<ResponseDto>> UpdateCategoryById(Guid Id, [FromBody] CategoryDto Category)
+        {
+            var result = await sender.Send(new UpdateCategoryCommand(Category, Id));
+
+            return result.Match<ActionResult<ResponseDto>>(
+                s => Ok(new ResponseBuilder()
+                    .Succeeded()
+                    .AddResult(s)
+                    .AddResultMessage("Category was successfully updated.")
+                    .Build()),
+                f =>
+                {
+                    if (f is NotFoundCategoryException exception)
+                        return new ResponseBuilder()
+                            .Failed()
+                            .AddErrorMessage(exception.Message)
+                            .Build();
+
+                    if (f is FailedValidationException validation)
+                        return new ResponseBuilder()
+                            .Failed()
+                            .AddErrorMessage(validation.Message)
+                            .AddErrors(validation.Errors.ToList())
+                            .Build();
 
                     return StatusCode(500, new ResponseBuilder()
                         .Failed()
