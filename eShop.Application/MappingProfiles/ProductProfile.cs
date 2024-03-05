d0d4120143d3bb00646b21af0f82430a6432db77
A.Osavoliuk <sasha.osavoll11@gmail.com>
2024-03-05 16:29:52 +0200

    Added mapping for products

---
+++ eShop.Application/MappingProfiles/ProductProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using eShop.Domain.DTOs.Requests;
+using eShop.Domain.Entities;
+
+namespace eShop.Application.MappingProfiles
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<ProductDto, ProductEntity>();
+        }
+    }
+}
