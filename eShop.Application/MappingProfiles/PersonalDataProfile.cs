6d8d9af2fccb4590524facc924343edb0c1af597
A.Osavoliuk <sasha.osavoll11@gmail.com>
2024-03-11 15:33:57 +0200

    Added mapping for personal data

---
+++ eShop.Application/MappingProfiles/PersonalDataProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using eShop.Domain.DTOs;
+using eShop.Domain.DTOs.Requests;
+
+namespace eShop.Application.MappingProfiles
+{
+    public class PersonalDataProfile : Profile
+    {
+        public PersonalDataProfile()
+        {
+            CreateMap<PersonalDataDto, ChangePersonalDataRequestDto>();
+        }
+    }
+}
