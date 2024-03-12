d0176282730be5b52393958cacaefcfbb56c0aa9
A.Osavoliuk <sasha.osavoll11@gmail.com>
2024-03-12 15:36:38 +0200

    Added LabeledText components, rework data showing

--- eShop.Domain/DTOs/PersonalInformationDto.cs
+++ eShop.Domain/DTOs/PersonalInformationDto.cs
@@ -8,6 +8,6 @@
         public string Gender { get; set; } = "";
         public string Email { get; set; } = "";
         public string PhoneNumber { get; set; } = "";
-        public DateTime? DateOfBirth { get; set; }
+        public DateTime DateOfBirth { get; set; } = new DateTime(1980, 1, 1);
     }
 }
