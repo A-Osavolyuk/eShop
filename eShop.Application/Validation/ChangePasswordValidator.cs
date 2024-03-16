9ddb15643a91c75439fb91a53b0c8d9a1b3e4817
A.Osavoliuk <sasha.osavoll11@gmail.com>
2024-03-16 14:42:17 +0200

    Added validation for ChangePasswordRequestDto

---
+++ eShop.Application/Validation/ChangePasswordValidator.cs
@@ -0,0 +1,27 @@
+﻿using eShop.Domain.DTOs.Requests;
+using FluentValidation;
+
+namespace eShop.Application.Validation
+{
+    public class ChangePasswordValidator : AbstractValidator<ChangePasswordRequestDto>
+    {
+        public ChangePasswordValidator()
+        {
+            RuleFor(p => p.OldPassword)
+                .NotEmpty().WithMessage("You must enter your old password.");
+
+            RuleFor(p => p.NewPassword)
+                .NotEmpty().WithMessage("New Password is must.")
+                .MinimumLength(8).WithMessage("New Password must be at least 8 characters long.")
+                .MaximumLength(32).WithMessage("New Password cannot be longer then 32 characters.")
+                .Matches("[A-Z]").WithMessage("New Password must contain at least one uppercase letter.")
+                .Matches("[a-z]").WithMessage("New Password must contain at least one lowercase letter.")
+                .Matches("[0-9]").WithMessage("New Password must contain at least one numeric digit.")
+                .Matches("[^a-zA-Z0-9]").WithMessage("New Password must contain at least one special character.");
+
+            RuleFor(p => p.ConfirmNewPassword)
+                .NotEmpty().WithMessage("You must confirm your new password.")
+                .Equal(x => x.NewPassword).WithMessage("Must be the same with new password.");
+        }
+    }
+}
