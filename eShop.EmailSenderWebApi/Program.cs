157118396d87301cb1bca324ab9e6d9a6e27b3c0
A.Osavoliuk <sasha.osavoll11@gmail.com>
2024-03-24 11:25:39 +0200

    Removed rabbit mq container

--- eShop.EmailSenderWebApi/Program.cs
+++ eShop.EmailSenderWebApi/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
+builder.AddRabbitMQ("RabbitMQ");
+
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
--- eShop/eShop/eShop.AppHost/Program.cs
+++ eShop/eShop/eShop.AppHost/Program.cs
@@ -3,15 +3,12 @@ var builder = DistributedApplication.CreateBuilder(args);
 var azureStorage = builder.AddAzureStorage("AzureStorage")
     .AddBlobs("AvatarsStorage");
 
-var rabbitMq = builder.AddRabbitMQ("MessageBus", 9001);
-
 var sqlServer = builder.AddSqlServerContainer("SqlServer", "Password_2024", 8500)
     .AddDatabase("AuthDB")
     .AddDatabase("ProductDB");
 
 var authApi = builder.AddProject<Projects.eShop_AuthWebApi>("AuthWebApi")
     .WithReference(sqlServer)
-    .WithReference(rabbitMq);
 
 var productApi = builder.AddProject<Projects.eShop_ProductWebApi>("ProductWebApi")
     .WithReference(sqlServer)
