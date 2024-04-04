1ff080fc7bc1b42d07638875226f4825fe8bc689
A.Osavoliuk <sasha.osavoll11@gmail.com>
2024-04-04 17:08:18 +0300

    Added ChangeEmailComponent dialog

---
+++ eShop.Application/Validation/ChangeEmailValidator.cs
@@ -0,0 +1,13 @@
+﻿using eShop.Domain.DTOs.Requests;
+using FluentValidation;
+
+namespace eShop.Application.Validation
+{
+    public class ChangeEmailValidator : AbstractValidator<ChangeEmailRequest>
+    {
+        public ChangeEmailValidator()
+        {
+            RuleFor(x => x.NewEmail).EmailAddress().WithMessage("Invalid email address format.");
+        }
+    }
+}
