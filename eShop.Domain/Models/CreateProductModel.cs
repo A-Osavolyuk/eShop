a4fe4d66055b3bf24e24401e9f2bfe21395b24a6
A.Osavoliuk <sasha.osavoll11@gmail.com>
2024-06-02 15:41:04 +0300

    Added model for product creation, add validation for CreateProductModel, fixed issue with validation

---
+++ eShop.Domain/Models/CreateProductModel.cs
@@ -0,0 +1,22 @@
+﻿using eShop.Domain.Enums;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace eShop.Domain.Models
+{
+    public class CreateProductModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public Categoty Category { get; set; }
+        public string Compound { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public Currency Currency { get; set; }
+        public List<string> Images { get; set; } = new List<string>();
+        public IReadOnlyList<IBrowserFile> Files { get; set; } = [];
+        public Guid BrandId { get; set; } = Guid.Empty;
+        public HashSet<ProductSize> Sizes { get; set; } = new();
+        public ProductColor Color { get; set; }
+        public Audience Audience { get; set; }
+    }
+}
