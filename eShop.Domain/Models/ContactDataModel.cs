b1dadbffe64dcff12a24dcf93a0361b179ca987d
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2024-10-21 19:43:55 +0300

    Deleted redundant models

--- eShop.Domain/Models/ContactDataModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace eShop.Domain.Models
-{
-    public class ContactDataModel
-    {
-        public string Email { get; set; } = string.Empty;
-        public string PhoneNumber { get; set; } = string.Empty;
-    }
-}
