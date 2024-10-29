63ba41a4e13e20a0a0191458c799bcc12faa6bed
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2024-10-29 17:16:31 +0200

    Added UpdateFavoritesCommand to update existing favorites, added storing of favorites in MongoDB, added FavoritesController

---
+++ eShop.CartWebApi/Controllers/FavoritesController.cs
@@ -0,0 +1,22 @@
+﻿using eShop.CartWebApi.Commands.Favorites;
+using eShop.Domain.Requests.Cart;
+
+namespace eShop.CartWebApi.Controllers;
+
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+[ApiVersion("1.0")]
+public class FavoritesController(ISender sender) : ControllerBase
+{
+    private readonly ISender sender = sender;
+    
+    [HttpPut("update-favorites")]
+    public async ValueTask<ActionResult<ResponseDTO>> UpdateCartAsync([FromBody] UpdateFavoritesRequest request)
+    {
+        var response = await sender.Send(new UpdateFavoritesCommand(request));
+
+        return response.Match(
+            s => Ok(new ResponseBuilder().Succeeded().WithResult(s).Build()),
+            ExceptionHandler.HandleException);
+    }
+}
