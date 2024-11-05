beaef03614b6673dd9f694cb8017070fcf9fd9ef
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2024-11-05 13:25:58 +0200

    Added logic to ProductService

--- eShop.Domain/Interfaces/IProductService.cs
+++ eShop.Domain/Interfaces/IProductService.cs
@@ -1,7 +1,16 @@
-﻿namespace eShop.Domain.Interfaces
+﻿using eShop.Domain.DTOs;
+using eShop.Domain.Requests.Product;
+
+namespace eShop.Domain.Interfaces
 {
     public interface IProductService
     {
-
+        public ValueTask<ResponseDTO> CreateProductAsync(CreateProductRequest request);
+        public ValueTask<ResponseDTO> UpdateProductAsync(UpdateProductRequest request);
+        public ValueTask<ResponseDTO> DeleteProductAsync(DeleteProductRequest request);
+        public ValueTask<ResponseDTO> GetProductsAsync();
+        public ValueTask<ResponseDTO> GetProductByNameAsync(string name);
+        public ValueTask<ResponseDTO> GetProductByArticleAsync(string article);
+        public ValueTask<ResponseDTO> GetProductByIdAsync(Guid id);
     }
 }
--- eShop.Infrastucture/Services/ProductService.cs
+++ eShop.Infrastucture/Services/ProductService.cs
@@ -1,4 +1,7 @@
-﻿using eShop.Domain.Interfaces;
+﻿using eShop.Domain.DTOs;
+using eShop.Domain.Enums;
+using eShop.Domain.Interfaces;
+using eShop.Domain.Requests.Product;
 using Microsoft.Extensions.Configuration;
 
 namespace eShop.Infrastructure.Services
@@ -9,6 +12,37 @@ namespace eShop.Infrastructure.Services
     {
         private readonly IHttpClientService clientService = clientService;
         private readonly IConfiguration configuration = configuration;
-        
+
+        public async ValueTask<ResponseDTO> CreateProductAsync(CreateProductRequest request) =>
+            await clientService.SendAsync(
+                new RequestDto(Url: $"{configuration["Services:Gateway"]}/api/v1/Prodcuts/create-product",
+                    Data: request, Method: HttpMethods.POST));
+
+        public async ValueTask<ResponseDTO> UpdateProductAsync(UpdateProductRequest request) =>
+            await clientService.SendAsync(
+                new RequestDto(Url: $"{configuration["Services:Gateway"]}/api/v1/Prodcuts/update-product",
+                    Data: request, Method: HttpMethods.PUT));
+
+        public async ValueTask<ResponseDTO> DeleteProductAsync(DeleteProductRequest request) =>
+            await clientService.SendAsync(
+                new RequestDto(Url: $"{configuration["Services:Gateway"]}/api/v1/Prodcuts/delete-product",
+                    Data: request, Method: HttpMethods.DELETE));
+
+        public async ValueTask<ResponseDTO> GetProductsAsync() => await clientService.SendAsync(
+            new RequestDto(Url: $"{configuration["Services:Gateway"]}/api/v1/Prodcuts/get-products",
+                Method: HttpMethods.GET));
+
+        public async ValueTask<ResponseDTO> GetProductByNameAsync(string name) => await clientService.SendAsync(
+            new RequestDto(Url: $"{configuration["Services:Gateway"]}/api/v1/Prodcuts/get-product-by-name/{name}",
+                Method: HttpMethods.GET));
+
+        public async ValueTask<ResponseDTO> GetProductByArticleAsync(string article) => await clientService.SendAsync(
+            new RequestDto(
+                Url: $"{configuration["Services:Gateway"]}/api/v1/Prodcuts/get-product-by-article/{article}",
+                Method: HttpMethods.GET));
+
+        public async ValueTask<ResponseDTO> GetProductByIdAsync(Guid id) => await clientService.SendAsync(
+            new RequestDto(Url: $"{configuration["Services:Gateway"]}/api/v1/Prodcuts/get-product-by-id/{id}",
+                Method: HttpMethods.GET));
     }
 }
