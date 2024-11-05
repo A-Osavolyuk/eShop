3225d7d0b1ee095554a9d0afd60e43c712dc55ab
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2024-11-05 13:34:26 +0200

    Added loading of product on first render of main page

--- eShop.Infrastucture/Services/ProductService.cs
+++ eShop.Infrastucture/Services/ProductService.cs
@@ -15,34 +15,34 @@ namespace eShop.Infrastructure.Services
 
         public async ValueTask<ResponseDTO> CreateProductAsync(CreateProductRequest request) =>
             await clientService.SendAsync(
-                new RequestDto(Url: $"{configuration["Services:Gateway"]}/api/v1/Prodcuts/create-product",
+                new RequestDto(Url: $"{configuration["Services:Gateway"]}/api/v1/Products/create-product",
                     Data: request, Method: HttpMethods.POST));
 
         public async ValueTask<ResponseDTO> UpdateProductAsync(UpdateProductRequest request) =>
             await clientService.SendAsync(
-                new RequestDto(Url: $"{configuration["Services:Gateway"]}/api/v1/Prodcuts/update-product",
+                new RequestDto(Url: $"{configuration["Services:Gateway"]}/api/v1/Products/update-product",
                     Data: request, Method: HttpMethods.PUT));
 
         public async ValueTask<ResponseDTO> DeleteProductAsync(DeleteProductRequest request) =>
             await clientService.SendAsync(
-                new RequestDto(Url: $"{configuration["Services:Gateway"]}/api/v1/Prodcuts/delete-product",
+                new RequestDto(Url: $"{configuration["Services:Gateway"]}/api/v1/Products/delete-product",
                     Data: request, Method: HttpMethods.DELETE));
 
         public async ValueTask<ResponseDTO> GetProductsAsync() => await clientService.SendAsync(
-            new RequestDto(Url: $"{configuration["Services:Gateway"]}/api/v1/Prodcuts/get-products",
+            new RequestDto(Url: $"{configuration["Services:Gateway"]}/api/v1/Products/get-products",
                 Method: HttpMethods.GET));
 
         public async ValueTask<ResponseDTO> GetProductByNameAsync(string name) => await clientService.SendAsync(
-            new RequestDto(Url: $"{configuration["Services:Gateway"]}/api/v1/Prodcuts/get-product-by-name/{name}",
+            new RequestDto(Url: $"{configuration["Services:Gateway"]}/api/v1/Products/get-product-by-name/{name}",
                 Method: HttpMethods.GET));
 
         public async ValueTask<ResponseDTO> GetProductByArticleAsync(string article) => await clientService.SendAsync(
             new RequestDto(
-                Url: $"{configuration["Services:Gateway"]}/api/v1/Prodcuts/get-product-by-article/{article}",
+                Url: $"{configuration["Services:Gateway"]}/api/v1/Products/get-product-by-article/{article}",
                 Method: HttpMethods.GET));
 
         public async ValueTask<ResponseDTO> GetProductByIdAsync(Guid id) => await clientService.SendAsync(
-            new RequestDto(Url: $"{configuration["Services:Gateway"]}/api/v1/Prodcuts/get-product-by-id/{id}",
+            new RequestDto(Url: $"{configuration["Services:Gateway"]}/api/v1/Products/get-product-by-id/{id}",
                 Method: HttpMethods.GET));
     }
 }
