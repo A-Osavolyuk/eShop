832d16e79e2f276f9a1b9748da1656bb902637ba
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-01-02 15:13:12 +0200

    Fixed issue with mapping

--- eShop.Application/Mapping/PersonalDataMapper.cs
+++ eShop.Application/Mapping/PersonalDataMapper.cs
@@ -6,17 +6,6 @@ namespace eShop.Application.Mapping;
 
 public static class PersonalDataMapper
 {
-    public static ChangePersonalDataResponse ToChangePersonalDataResponse(PersonalDataEntity entity)
-    {
-        return new ChangePersonalDataResponse()
-        {
-            FirstName = entity.FirstName,
-            LastName = entity.LastName,
-            Gender = entity.Gender,
-            DateOfBirth = entity.DateOfBirth
-        };
-    }
-    
     public static PersonalDataResponse ToPersonalDataResponse(PersonalDataEntity entity)
     {
         return new PersonalDataResponse()
@@ -47,17 +36,6 @@ public static class PersonalDataMapper
             LastName = model.LastName,
             Gender = model.Gender,
             DateOfBirth = model.DateOfBirth
-        };
-    }
-
-    public static PersonalDataModel ToPersonalDataModel(ChangePersonalDataResponse response)
-    {
-        return new PersonalDataModel()
-        {
-            FirstName = response.FirstName,
-            LastName = response.LastName,
-            Gender = response.Gender,
-            DateOfBirth = response.DateOfBirth
         };
     }
 }
