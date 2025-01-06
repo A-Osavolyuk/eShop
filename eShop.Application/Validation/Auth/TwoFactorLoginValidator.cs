b51ff2e6aa40739ab989c03d792cd2ed4550e87f
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-01-06 16:37:02 +0200

    Added form text formation, reworked 2fa login page

--- eShop.Application/Validation/Auth/TwoFactorLoginValidator.cs
+++ eShop.Application/Validation/Auth/TwoFactorLoginValidator.cs
@@ -4,6 +4,15 @@ namespace eShop.Application.Validation.Auth;
 
 public class TwoFactorLoginValidator : AbstractValidator<TwoFactorAuthenticationLoginRequest>
 {
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+    {
+        var result = await ValidateAsync(ValidationContext<TwoFactorAuthenticationLoginRequest>
+            .CreateWithOptions((TwoFactorAuthenticationLoginRequest)model, x => x.IncludeProperties(propertyName)));
+        if (result.IsValid)
+            return Array.Empty<string>();
+        return result.Errors.Select(e => e.ErrorMessage);
+    };
+    
     public TwoFactorLoginValidator()
     {
         RuleFor(x => x.Code)
